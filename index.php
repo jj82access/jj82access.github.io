860be452339de7dbf06fd76c1b21fba555b07865
jj82access <166193234+jj82access@users.noreply.github.com>
2024-07-12 22:26:53 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <style>
+    a {
+/*       color:red; */
+/*       text-align:center; */
+/*       border:1px solid blue; */
+      text-decoration: none;
+    }
+    h1 {
+/*       border:1px solid green; */
+/*       text-align:center; */
+    }
+    h2 {
+/*       border:1px solid green; */
+/*       text-align:center; */
+    }
+  </style>
+</head>
+<body>
+  <h1>JJACCESS</h1>
+  <h2><a href="booking-note.html">예약 노트</a></h2>
+  <h2><a href="privacy-policy.html">Privacy Policy</a></h2>
+  <h2><a href="kp.html">KP</a></h2>
+</body>
+</html>
+
+
+<!--     <title>Document</title>
 </head>
 <body>
     <a href="http://dev.mxweb.kr:8438/main/main.php">메인화면으로 가기</a>
 </body>
-</html>
+</html> -->
