7f562400ff763c6cbfa4560672cef3d81618dbe3
jj82access <166193234+jj82access@users.noreply.github.com>
2024-07-12 22:23:53 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="http://dev.mxweb.kr:8438/main/main.php">메인화면으로 가기</a>
+</body>
+</html>
