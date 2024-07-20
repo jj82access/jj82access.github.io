fbd8e9e17fb50d40910e718e7e93c7d48c51f95b
jj82access <166193234+jj82access@users.noreply.github.com>
2024-07-20 16:46:14 -0400

    Add files via upload

---
+++ reference/home_page.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+  <title>예약 노트</title>
+  <meta charset="utf-8">
+</head>
+<body>
+  <h1>예약 노트</h1>
+  <h2>예약 노트</h2>
+  <?php
+    echo "333";
+    echo "<br>";
+    print(33);
+    echo "<br>";
+    $a = 10;
+    print($a + 1);
+    echo "<br>";
+  ?>
+  <?php
+    echo "Hello World";
+    echo "<br>";
+  ?>
+</body>
+</html>
